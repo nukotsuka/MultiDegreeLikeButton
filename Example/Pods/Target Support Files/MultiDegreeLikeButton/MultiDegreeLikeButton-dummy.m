ca47f58c204d352de0c1fc22282b5386d819318c
nukotsuka <nukotsuka@gmail.com>
2018-10-20 13:28:06 +0900

    Initial commit

---
+++ Example/Pods/Target Support Files/MultiDegreeLikeButton/MultiDegreeLikeButton-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_MultiDegreeLikeButton : NSObject
+@end
+@implementation PodsDummy_MultiDegreeLikeButton
+@end
---
+++ hoge.swift
@@ -0,0 +1,8 @@
+//
+//  hoge.swift
+//  FBSnapshotTestCase
+//
+//  Created by 中塚貴大 on 2018/10/20.
+//
+
+import Foundation
